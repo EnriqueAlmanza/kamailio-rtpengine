2c9dee8d29a0025b0fbf3d26c29d7233e61e77c1
Enrique Almanza <almanza_119@hotmail.com>
2023-01-24 00:06:23 +0100

    added install-prereq template

---
+++ install-prereq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+###################################################
+###This script install the prerequisite software###
+###################################################
+clear
+echo "Configure Kamailio with the following IP address? (y/n)"
