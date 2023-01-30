34eaf06b33cddc949afa2680a9cd512f4913b5bf
Enrique Almanza <almanza_119@hotmail.com>
2023-01-30 02:57:02 +0100

    added webrtc ad refactored the master install script

--- master_install.sh
+++ master_install.sh
@@ -1,62 +1,106 @@
 #!/bin/bash
-############################
-###Kamailio configuration###
-############################
+##########################################
+###Kamailio and RTPengine configuration###
+##########################################
+function create_rtpengineconf() {
+  RTPconf=".rtpengine/rtpengine.conf"
+  /bin/cat <<EOM >$RTPconf
+[rtpengine]
+table = -1
+interface = $IP
+listen-ng = localhost:2223
+listen-http = localhost:2225
+listen-cli = localhost:2224
+timeout = 60
+silent-timeout = 3600
+tos = 184
+port-min = 30000
+port-max = 40000
+recording-dir = /var/spool/rtpengine
+recording-method = proc
+
+[rtpengine-testing]
+table = -1
+interface = $IP
+listen-ng = 2223
+foreground = true
+log-stderr = true
+log-level = 7
+EOM
+}
+
+function create_kamailio_address() {
+  kamAddress=".config_kamailio/kamailio-address.cfg"
+  /bin/cat <<EOM >$kamAddress
+#!KAMAILIO
+#!substdef "!MY_IP_ADDR!$IP!g"
+#!substdef "!MY_DOMAIN!debian-test.youcon.voip!g"
+#!substdef "!MY_WS_PORT!88080!g"
+#!substdef "!MY_WSS_PORT!44443!g"
+#!substdef "!MY_WS_ADDR!tcp:MY_IP_ADDR:MY_WS_PORT!g"
+#!substdef "!MY_WSS_ADDR!tls:MY_IP_ADDR:MY_WSS_PORT!g"
+EOM
+}
+
+function running_containers() {
+  echo "Using docker compose to start the containers"
+  docker compose build --no-cache
+  docker compose up -d
+}
+
 clear
-echo "Configure Kamailio with the following IP address? (y/n)"
-ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-read decision
+#echo "Configure Kamailio with the following IP address? (y/n)"
+#ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
+IP=$(ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+{
+  prompt="Configure Kamailio with the address $IP? (y/n): "
+  read -p "$prompt" decision
+  echo -e "Answer: [$decision]"
+}
 if [[ $decision = "n" ]]
 then
- echo "Enter the new IP - e.g., 192.168.10.3: "
- read IP
- echo ${IP} >> IP
- while IFS= read -r IP; do
-  echo "Configuring IP ${IP} to kamailio-address.cfg file"
-  echo "MY_IP_ADDR=${IP}" >> kamailio_address.cfg
- done < IP
- rm IP
+  {
+    prompt="Enter the new Kamailio IP - e.g., 192.168.10.3: "
+    read -p "$prompt" IP
+    echo -e "New IP: [$IP]"
+  }
+  echo -e "Generating kamailio-address.cfg file \n"
+  create_kamailio_address $IP
 elif [[ $decision = "y" ]]
 then
- ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' >> IP
- while IFS= read -r IP; do
-  echo "Configuring Kamailio with IP: $IP"
-  echo "MY_IP_ADDR=${IP}" >> kamailio_address.cfg
- done < IP
- rm IP
+  IP=$(ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+  echo -e "Generating kamailio-address.cfg file \n"
+  create_kamailio_address $IP
 else
- echo "Please, enter a correct option (y/n)"
+  echo "Please, enter a correct option (y/n)"
+  exit
 fi
 #############################
 ###RTPengine configuration###
 #############################
-echo "Configure RTPengine with the following IP address? (y/n)"
-ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-read decision
+#echo "Configure RTPengine with the following IP address? (y/n)"
+{
+  prompt="Configure RTPengine with the address $IP? (y/n): "
+  read -p "$prompt" decision
+  echo -e "Answer: [$decision]"
+}
 if [[ $decision = "n" ]]
 then
- echo "Enter the new IP - e.g., 192.168.10.3: "
- read IP
- echo ${IP} >> IP
- while IFS= read -r IP; do
-  echo "Configuring IP ${IP} to kamailio-address.cfg file"
-  echo "modparam("rtpengine", "rtpengine_sock", "udp:${IP}:2223")" >> kamailio_address.cfg
- done < IP
- rm IP
+  {
+    prompt="Enter the new RTPengine IP- e.g., 192.168.10.3: "
+    read -p "$prompt" IP
+    echo -e "New IP: [$IP]"
+  }
+  echo -e "Generating rtpengine.conf file \n"
+  create_rtpengineconf $IP
+  running_containers
 elif [[ $decision = "y" ]]
 then
- ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' >> IP
- while IFS= read -r IP; do
-  echo "Configuring RTPengine with IP: $IP"
-  echo "modparam("rtpengine", "rtpengine_sock", "udp:${IP}:2223")" >> kamailio_address.cfg
- done < IP
- rm IP
- docker-compose -f docker-compose.yaml up -d
- docker rm -f rtpengine
- docker run --network host --privileged  --mount source=kamailio-rtpengine_rtpengine,target=/etc/rtpengine --name rtpengine -itd kamailio-rtpengine-rtpengine
-
- docker rm -f kamailio
- docker run --network host --privileged --mount source=kamailio-rtpengine_kamailio,target=/etc/kamailio --mount source=kamailio-rtpengine_db,target=/var/lib/mysql --name kamailio -itd kamailio-rtpengine-kamailio
+  IP=$(ip add | sed -En 's/127.0.0.1//;s/172.17.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+  echo "Generating rtpengine.conf file"
+  create_rtpengineconf $IP
+  running_containers
 else
- echo "Please, enter a correct option. Terminating..."
+  echo "Please, enter a correct option. Terminating..."
+  exit
 fi
